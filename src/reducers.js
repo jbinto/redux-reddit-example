e0cfcc86016b1e049a544ae757d47905207b6915
Jesse Buchanan <hello@jessebuchanan.ca>
2015-12-02 05:06:17 -0500

    make eslint happy, switch indentation is gross tho

--- src/reducers.js
+++ src/reducers.js
@@ -1,46 +1,49 @@
 import { combineReducers } from 'redux';
-import { SELECT_REDDIT } from '../src/actions'
+import { SELECT_REDDIT } from '../src/actions';
 
-export const selectedReddit = (state='javascript', action) => {
+export const selectedReddit = (state = 'javascript', action) => {
   // XXX switch action.type???
   switch (action.type) {
-    case SELECT_REDDIT:
-      return action.reddit;
-    default:
-      return state;
+  case SELECT_REDDIT:
+    return action.reddit;
+  default:
+    return state;
   }
-}
+};
 
 export const posts = (state = {
   isFetching: false,
   didInvalidate: false,
-  items: []
+  items: [],
 }, action) => {
   switch (action.type) {
-    case 'INVALIDATE_REDDIT':
-      return Object.assign({}, state, {
-        didInvalidate: true
-      });
-    case 'REQUEST_POSTS':
-      return Object.assign({}, state, {
-        isFetching: true,
-        // why even track invalidation?
-        // Guess: enable/disable refresh button?
-        didInvalidate: false
-      });
-    case 'RECEIVE_POSTS':
-      return Object.assign({}, state, {
-        isFetching: false,
-        didInvalidate: false,
-        updatedAt: action.receivedAt,
-        items: action.posts
-      });
-    default:
-      return state;
+  case 'INVALIDATE_REDDIT':
+    return Object.assign({}, state, {
+      didInvalidate: true,
+    });
+  case 'REQUEST_POSTS':
+    return Object.assign({}, state, {
+      isFetching: true,
+      // why even track invalidation?
+      // Guess: enable/disable refresh button?
+      didInvalidate: false,
+    });
+  case 'RECEIVE_POSTS':
+    return Object.assign({}, state, {
+      isFetching: false,
+      didInvalidate: false,
+      updatedAt: action.receivedAt,
+      items: action.posts,
+    });
+  default:
+    return state;
   }
-}
+};
 
+export const postsByReddit = (state = {}, action) => {
+  return state;
+};
 
 export const rootReducer = combineReducers({
-  selectedReddit
+  selectedReddit,
 });
